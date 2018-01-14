2659f8381ec113d7e41d25c481d717bb3a0c253b
banxuelai160714 <banxuelai>
2018-01-14 11:20:48 +0800

    dict

--- first.py
+++ first.py
@@ -7,6 +7,22 @@ print str[0]
 print str[2:7]
 print str[2:]
 print sys.argv
+
+list = ['first',1,'second',2]
+tinylist = [3,'ff']
+
+print list+tinylist
+
+dict = {}
+dict['one'] = "this is one"
+dict[2] = "this is two"
+tinylist = {'name':'jhon','phone':'15690018743','dept':'3.3'}
+
+print dict['one']
+print dict[2]
+print tinylist
+print tinylist.keys()
+print tinylist.values()
 exit(0)
 
 
